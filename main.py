439164c97530398f1da2b58bb5faeca932f70900
yygjyhyniuhjnedcs <k1340537@students.katyisd.org>
2026-05-07 13:27:00 +0000

    Added binary search and merge sort

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
+import sys
+
+
 class StockManger:
     
     def __init__(self):
-        self.itemList = []
+        self.item_list = []
     
     def search(self, search_term):
         low = 0
-        high = len(self.itemList) - 1
+        high = len(self.item_list) - 1
         while low <= high:
             mid = (low + high) // 2
-            guess = self.itemList[mid]
+            guess = self.item_list[mid]
             if search_term.lower() in guess.name.lower():
-                return self.itemList[mid]
+                return self.item_list[mid]
             if guess.name.lower() > search_term.lower():
                 high = mid - 1
             else:
                 low = mid + 1
         return None
+    
+    def _sort_items(self, list=None):
+        if list is None:
+            list = self.item_list
+
+        half1 = list[:int(len(list)/2):]
+        half2 = list[int(len(list)/2)::]
+
+        if not half1:
+            return half2     
+        return self._merge(self._sort_items(half1), self._sort_items(half2))
+    
+    def _merge(self, list1, list2):
+        temp_list = []
+        index1 = 0
+        index2 = 0
+        while index1 < len(list1) or index2 < len(list2):
+            item1 = chr(0x10FFFF) 
+            item2 = chr(0x10FFFF)
+            
+            if list1 is not None and len(list1) > index1:
+                item1 = list1[index1].name
+            if list2 is not None and len(list2) > index2:
+                item2 = list2[index2].name
+
+            if item1 > item2:
+                temp_list.append(list2[index2])
+                index2 += 1
+            elif item1 <= item2:
+                temp_list.append(list1[index1])
+                index1 += 1
+        return temp_list
+    
+    def add(self, item):
+        self.item_list.append(item)
+        self.item_list = self._sort_items(self.item_list)
     
     def timeLeft(self):
         return 
@@ -33,7 +72,12 @@ class Item():
         self.history.append(self.stock)
 
 s = StockManger()
-s.itemList.append(Item("Chad Juice", 1))
-s.itemList.append(Item("Chud Juice", 2))
-s.itemList.append(Item("Sigma Juice", 3))
-print(s.search("cha").name)
+s.add(Item("Chud Juice", 1))
+s.add(Item("Chad Juice", 2))
+s.add(Item("Sigma Juice", 3))
+s.add(Item("Alpha juice", 0))
+for item in s.item_list:
+    print(item.name)
+print()
+print(f"Seach term: \"s\"\nResult: {s.search("s").name}")
+
